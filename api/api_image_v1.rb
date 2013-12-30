14b4b45a894fc4342eeb87c2822e308fe89ddb9e
Tom McSweeney <tjmcs@bendbroadband.com>
2013-12-30 14:49:42 -0800

    Adds limits for access to /image (razor server only) and /image/{path} (razor subnet only) endpoints of our grape-style RESTful API

--- api/api_image_v1.rb
+++ api/api_image_v1.rb
@@ -50,20 +50,49 @@ module Razor
           def is_uuid?(string_)
             string_ =~ /[a-fA-F0-9]{8}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{12}/
           end
+
+          def request_is_from_razor_server(ip_addr)
+            Razor::WebService::Utils::request_from_razor_server?(ip_addr)
+          end
+
+          def request_is_from_razor_subnet(ip_addr)
+            Razor::WebService::Utils::request_from_razor_subnet?(ip_addr)
+          end
+
         end
 
         resource :image do
 
           # GET /image
           # Query for images.
+          before do
+            # only test if directly accessing the /config resource
+            if env["PATH_INFO"].match(/image$/)
+              # only allow access to configuration resource from the razor server
+              unless request_is_from_razor_server(env['REMOTE_ADDR'])
+                error!({ error: "Remote Access Forbidden",
+                         detail: "Access to /image resource is only allowed from Razor server",
+                       }, 403)
+              end
+            end
+          end
           get do
             image_slice = ProjectRazor::Slice.new
             Razor::WebService::Response.new(200, 'OK', 'Success.', image_slice.get_object("images", :images))
           end     # end GET /image
 
           resource '/:path', requirements: { path: /.*/ } do
-            # GET /image/{os_type}/{uuid}/{action}/{file}
+            # GET /image/{path}
             # Query for file from an image (by path)
+            before do
+              # only allow access to this resource from the Razor subnet
+              unless request_is_from_razor_subnet(env['REMOTE_ADDR'])
+                env['api.format'] = :text
+                error!({ error: "Remote Access Forbidden",
+                         detail: "Access to /image/{path} resource is not allowed from outside of the Razor subnet",
+                       }, 403)
+              end
+            end
             get do
               path = params[:path]
               filename = path.split(File::SEPARATOR)[-1]
