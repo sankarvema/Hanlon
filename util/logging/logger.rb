dff72a28ad17e1bce0ee72c3de662d76d1ccae15
sankar.vema <sans.vema@gmail.com>
2014-05-07 03:15:23 -0400

    logger extended with log_exception fn

--- util/logging/logger.rb
+++ util/logging/logger.rb
@@ -1,4 +1,4 @@
-require "logger"
+require "logging/new_logger"
 
 # ToDo::Sankar::Implement log level hardcoded for time being
 # this should come from config parameter {hanlon_log_level}
@@ -11,7 +11,6 @@ LOG_MAX_FILES = 10
 # It auto prefixes each log message with classname and method from which it was called using progname
 module ProjectHanlon::Logging
 
-
       # [Hash] holds the loggers for each instance that includes it
       @loggers = {}
 
@@ -19,9 +18,9 @@ module ProjectHanlon::Logging
       def logger
         classname = self.class.name
         methodname = caller[0][/`([^']*)'/, 1]
+
         @_logger ||= ProjectHanlon::Logging.logger_for(classname, methodname)
-        @_logger.progname = "#{classname}\##{methodname}"
-        puts "logger initialized #{@_logger.progname} with log level #{@_logger.level}"
+        @_logger.progname = "#{caller[0]}>>#{classname}\##{methodname}"
 
         @_logger
       end
@@ -48,14 +47,9 @@ module ProjectHanlon::Logging
 
         # Creates a logger instance
         def configure_logger_for(classname, methodname)
-          logger = Logger.new(get_log_path, shift_age = LOG_MAX_FILES, shift_size = LOG_MAX_SIZE)
+          logger = CustomLogger.new(get_log_path, shift_age = LOG_MAX_FILES, shift_size = LOG_MAX_SIZE)
           logger.level = get_log_level
           logger
-        end
-
-        def log_exception(ex)
-          logger.error "exception occured #{ex.message}"
-          logger.debug "#{ex.backtrace}"
         end
 
       end
