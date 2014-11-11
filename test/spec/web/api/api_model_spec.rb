253b56312becf3c19fc97972b2045763a4138f28
Russell Teague <mtnbikenc@gmail.com>
2014-11-11 10:56:44 -0500

    Added checks to ensure the proper model_uuid was returned for related operations.

--- test/spec/web/api/api_model_spec.rb
+++ test/spec/web/api/api_model_spec.rb
@@ -51,8 +51,7 @@ describe 'Hanlon::WebService::Model' do
         expect(parsed['http_err_code']).to eq(201)
         expect(parsed['errcode']).to eq(0)
         # save the created UUID for later use
-        response_hash = parsed['response']
-        $spec_uuid = response_hash['@uuid']
+        $model_uuid = parsed['response']['@uuid']
       end
     end   # end POST /model
 
@@ -104,7 +103,7 @@ describe 'Hanlon::WebService::Model' do
 
       describe 'GET /model/{uuid}' do
         it 'Returns details for a specific model instance (by UUID)' do
-          uri = URI.parse(hnl_uri + '/model/' + $spec_uuid)
+          uri = URI.parse(hnl_uri + '/model/' + $model_uuid)
           http_client = Net::HTTP.new(uri.host, uri.port)
           request = Net::HTTP::Get.new(uri.request_uri)
           # make the request
@@ -116,12 +115,14 @@ describe 'Hanlon::WebService::Model' do
           expect(parsed['result']).to eq('Ok')
           expect(parsed['http_err_code']).to eq(200)
           expect(parsed['errcode']).to eq(0)
+          # make sure we are getting the same model
+          expect(parsed['response']['@uuid']).to eq($model_uuid)
         end
       end   # end GET /model/{uuid}
 
       describe 'PUT /model/{uuid}' do
         it 'Updates a model instance (by UUID)' do
-          uri = URI.parse(hnl_uri + '/model/' + $spec_uuid)
+          uri = URI.parse(hnl_uri + '/model/' + $model_uuid)
 
           label = 'new_spec_test_model'
 
@@ -144,12 +145,14 @@ describe 'Hanlon::WebService::Model' do
           expect(parsed['result']).to eq('Updated')
           expect(parsed['http_err_code']).to eq(202)
           expect(parsed['errcode']).to eq(0)
+          # make sure we are updating the label
+          expect(parsed['response']['@label']).to eq(label)
         end
       end   # end PUT /model/{uuid}
 
       describe 'DELETE /model/{uuid}' do
         it 'Removes a model instance (by UUID)' do
-          uri = URI.parse(hnl_uri + '/model/' + $spec_uuid)
+          uri = URI.parse(hnl_uri + '/model/' + $model_uuid)
           http_client = Net::HTTP.new(uri.host, uri.port)
           request = Net::HTTP::Delete.new(uri.request_uri)
           # make the request
@@ -161,6 +164,8 @@ describe 'Hanlon::WebService::Model' do
           expect(parsed['result']).to eq('Removed')
           expect(parsed['http_err_code']).to eq(202)
           expect(parsed['errcode']).to eq(0)
+          # make sure we are returning the same model uuid
+          expect(parsed['response']).to include($model_uuid)
         end
       end   # end DELETE /model/{uuid}
 
