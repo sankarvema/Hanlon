49d5690f629d7e1500041cab46301ae0756325b1
sankarvema <sans.vema@gmail.com>
2014-11-24 01:33:08 -0800

    Hanlon migration error management added

---
+++ cli/migrate/errors.rb
@@ -0,0 +1,14 @@
+
+
+module ProjectHanlon
+  module Migrate
+    ErrorCodes =
+        {
+            :no_error                         => 0,
+            :invalid_arguments                => 1,
+            :missing_config_file              => 2,
+            :destination_db_not_empty         => 3,
+            :unknown_error                    => 99
+        }
+  end
+end
