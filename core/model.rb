9a32e3086f4e908616182ee29c7cbdc5b656e3a1
Russell Teague <mtnbikenc@gmail.com>
2015-03-09 14:59:24 -0400

    Updating Gemfile to support Ruby 2.2.0
    Several gems were outdated and would not function correctly when upgrading Ruby to 2.2.0.  Unused gems have been removed and several project files have been updated to reflect the changes.
    
    Fixes #330

--- core/model.rb
+++ core/model.rb
@@ -1,11 +1,3 @@
-require 'require_all'
-#require_rel 'model'
-
-# load all models
-#Dir[File.dirname(__FILE__) + "/model/**/*.rb"].each do |file|
-#  puts "---- require #{file}"
-#  require file
-#end
 
 require 'model/model_dep'
 
